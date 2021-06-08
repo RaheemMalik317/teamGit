bc9e085e042b65b8ba381d93d512bc2e065af758
Faith Fetzer <faithfetzer@Faiths-Air.attlocal.net>
2021-06-08 18:13:06 -0400

    weather display page

--- blue-badge-24/src/Weather/Weather.js
+++ blue-badge-24/src/Weather/Weather.js
@@ -15,19 +15,6 @@ const Weather = (props) =>{
 
     // console.log(lat,long);
 
-<<<<<<< HEAD
-    let apiURL = 'http://api.openweathermap.org/data/2.5/weather?lat=59.088112&lon=26.177601&appid=7a58d9b61109b78f87f3ebf6c6e68e0a';
-
-    // console.log(apiUrl);
-
-    // fetch(apiURL)
-    //     .then(res => res.json())
-    //     .then(response => setResults(response))
-    //     .catch(err => console.log(err))
-
-    // console.log(results);
-    // let temp = results.
-=======
     const fetchWeather = () => {
     let apiURL = `${baseUrl}lat=${lat}&lon=${long}&appid=${apiKey}&units=`;
     apiURL = imperial ? apiURL + `imperial` : apiURL +`metric`;
@@ -41,7 +28,6 @@ const Weather = (props) =>{
     console.log(results);
     // let temp = results.main.temp;
     // console.log(temp);
->>>>>>> 8d4575099cfa6208af9a9b9fe631bbf1384f93dd
 
     let setCentigrade =()=> {
             setImperial(false);
