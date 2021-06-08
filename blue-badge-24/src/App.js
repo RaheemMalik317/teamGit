4a8c5288821c8f89faf48b6ad0a983b127d0ce16
Faith Fetzer <faithfetzer@Faiths-Air.attlocal.net>
2021-06-08 15:54:04 -0400

    location function moved into app

--- blue-badge-24/src/App.js
+++ blue-badge-24/src/App.js
@@ -3,29 +3,29 @@ import './App.css';
 import Header from './site/Header';
 import DisplayArea from './site/DisplayArea';
 import Footer from './site/Header';
+// import Success from './site/GetLocation';
 
 function App() {
-
-
-
-  return (
-    <div className="App">
-      <Header/>
-      <DisplayArea/>
-      <Footer/>
-    </div>
-  );
-}
-
-export default App;
-
-function success(position) {
+  function success(position) {
   let latitude  = position.coords.latitude;
   let longitude = position.coords.longitude;
   console.log(latitude, longitude);
   localStorage.setItem('long', longitude);
   localStorage.setItem('lat', latitude);
   }
+  navigator.geolocation.getCurrentPosition(success);
+
+
+  return (
+    <div className="App">
+      <Header/>
+      <DisplayArea/>
+    </div>
+  );
+}
+
+export default App;
+
 
 
 // !THIS IS HOW TO CALL you cant just call success VVVV
--- blue-badge-24/src/Weather/Weather.js
+++ blue-badge-24/src/Weather/Weather.js
@@ -1,32 +1,34 @@
 import React, {useState} from 'react';
-import {Button} from 'reactstrap';
-// import GetLocation from '../site';
+// import {Button} from 'reactstrap';
 
-const baseUrl = 'api.openweathermap.org/data/2.5/weather?';
+
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?';
 const apiKey = '7a58d9b61109b78f87f3ebf6c6e68e0a';
 
 const Weather = (props) =>{
-    console.log(props);
-    const [results, setResults] =useState([])
+    // console.log(props);
+    const [results, setResults] =useState([]);
 
-    const lat= 59.088112;
-    const lon = 26.177601;
-    
-    
-    console.log(apiUrl);
+    const lat= localStorage.getItem('lat');
+    const long = localStorage.getItem('long');
 
-    const fetchWeather= () => {
-        let apiURL = `${baseURL}lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    console.log(lat,long)
 
-        fetch(apiURL)
-            .then(res => res.json())
-            .then(data => setResults(data.response))
-            .catch(err => console.log(err))
-    }
+    let apiURL = 'http://api.openweathermap.org/data/2.5/weather?lat=59.088112&lon=26.177601&appid=7a58d9b61109b78f87f3ebf6c6e68e0a';
+
+    // console.log(apiUrl);
+
+    // fetch(apiURL)
+    //     .then(res => res.json())
+    //     .then(response => setResults(response))
+    //     .catch(err => console.log(err))
+
+    // console.log(results);
+    // let temp = results.
 
     return (
         <div>
-            {results}
+            <h3>Current Weather</h3>
             {/* <Button onToggle={}></Button> */}
         </div>
     );
