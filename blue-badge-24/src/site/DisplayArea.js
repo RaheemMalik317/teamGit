5f4ef04d50f451a1d2b43f966fc98162c97e2957
Faith Fetzer <faithfetzer@Faiths-Air.attlocal.net>
2021-06-08 18:09:22 -0400

    weather buttons

--- blue-badge-24/src/site/DisplayArea.js
+++ blue-badge-24/src/site/DisplayArea.js
@@ -1,11 +1,11 @@
-import Weather from '../Weather/Weather';
+import WeatherDisplay from '../Weather/WeatherDisplay';
 import Nasa from '../Nasa/Nasa';
 import Ticketmaster from '../Ticketmaster/Ticketmaster';
 
 function DisplayArea() {
     return (
         <div>
-            <Weather/>
+            <WeatherDisplay/>
         </div>
     );
 }
