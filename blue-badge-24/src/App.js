ea026e0397e36d773175696dfe184f36342cc8df
Faith Fetzer <faithfetzer@Faiths-Air.attlocal.net>
2021-06-08 13:53:28 -0400

    built out folders

--- blue-badge-24/src/App.js
+++ blue-badge-24/src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import './App.css';
+import Header from './site/Header';
+import DisplayArea from './site/DisplayArea';
+import Footer from './site/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <DisplayArea/>
+      <Footer/>
     </div>
   );
 }
