b69313307c045e8f0ba922204c5c5c2964a31b36
Faith Fetzer <faithfetzer@Faiths-Air.attlocal.net>
2021-06-08 15:23:06 -0400

    added import for display area

--- blue-badge-24/src/Weather/Weather.js
+++ blue-badge-24/src/Weather/Weather.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button} from 'reactstrap';
 // import GetLocation from '../site';
 
-const baseUrl = `api.openweathermap.org/data/2.5/weather?`;
-const apiKey = `7a58d9b61109b78f87f3ebf6c6e68e0a`;
+const baseUrl = 'api.openweathermap.org/data/2.5/weather?';
+const apiKey = '7a58d9b61109b78f87f3ebf6c6e68e0a';
 
 const Weather = (props) =>{
     console.log(props);
+    const [results, setResults] =useState([])
 
-    let lat= getCurrentPosition(coords.latitude);
-    let lon = getCurrentPosition(coords.longitude);
+    const lat= 59.088112;
+    const lon = 26.177601;
     
-    let apiURL = `${baseURL}lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    
+    console.log(apiUrl);
 
     const fetchWeather= () => {
+        let apiURL = `${baseURL}lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
+        fetch(apiURL)
+            .then(res => res.json())
+            .then(data => setResults(data.response))
+            .catch(err => console.log(err))
     }
+
     return (
         <div>
-            <fetchWeather/>
-            <Button onToggle={}></Button>
+            {results}
+            {/* <Button onToggle={}></Button> */}
         </div>
     );
 }
