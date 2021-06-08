14912e247ef4e0162d49817dcd9315e06d7e4996
Faith Fetzer <faithfetzer@Faiths-Air.attlocal.net>
2021-06-08 14:37:39 -0400

    fixed reactstrap/bootsrap npm modules

--- blue-badge-24/src/Weather/Weather.js
+++ blue-badge-24/src/Weather/Weather.js
@@ -1,7 +1,25 @@
-function Weather() {
+import React from 'react';
+import {Button} from 'reactstrap';
+// import GetLocation from '../site';
+
+const baseUrl = `api.openweathermap.org/data/2.5/weather?`;
+const apiKey = `7a58d9b61109b78f87f3ebf6c6e68e0a`;
+
+const Weather = (props) =>{
+    console.log(props);
+
+    let lat= getCurrentPosition(coords.latitude);
+    let lon = getCurrentPosition(coords.longitude);
+    
+    let apiURL = `${baseURL}lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+    const fetchWeather= () => {
+
+    }
     return (
-        <div >
-        
+        <div>
+            <fetchWeather/>
+            <Button onToggle={}></Button>
         </div>
     );
 }
