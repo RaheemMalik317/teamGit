244439918c5cd423915a6f291e39e5d0dc553c3e
RaheemMalik317 <82741663+RaheemMalik317@users.noreply.github.com>
2021-06-08 15:37:48 -0400

    push

--- blue-badge-24/src/Ticketmaster/Ticketmaster.js
+++ blue-badge-24/src/Ticketmaster/Ticketmaster.js
@@ -1,4 +1,34 @@
-function Ticketmaster() {
+import React, {useState} from 'react'
+import TicketmasterResults from './TicketmasterResults'
+
+const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey='
+const key = '7drYHlAFPDVkMAGRvyAmhdDlCUWyZzGv'
+
+function Ticketmaster(props) {
+    console.log(props);
+
+let lat=getCurrentPosition(coords.latitude)
+let lon=getCurrentPosition(coords.longitude);
+
+let apiUrl = `${baseUrl}lat=${lat}&lon=${lon}&appid=${key}`;
+
+let fetchMaster = () => {
+    let apiUrl = `${baseUrl}lat=${lat}&lon=${lon}&appid=${key}`;
+
+    fetch(apiUrl)
+    .then(res => res.json())
+    .then(data => )
+}
+
+
+
+
+
+
+
+
+
+
     return (
         <div >
         
