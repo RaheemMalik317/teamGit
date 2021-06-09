93e01d4ff78feb17dbd0541a1d11fb2f316c706f
RaheemMalik317 <82741663+RaheemMalik317@users.noreply.github.com>
2021-06-08 20:31:27 -0400

    testing

--- blue-badge-24/src/Ticketmaster/Ticketmaster.js
+++ blue-badge-24/src/Ticketmaster/Ticketmaster.js
@@ -2,22 +2,25 @@ import React, {useState} from 'react'
 import {Button} from 'reactstrap'
 // import TicketmasterResults from './TicketmasterResults'
 
-const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey='
+const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?'
 const key = '7drYHlAFPDVkMAGRvyAmhdDlCUWyZzGv'
 
 function Ticketmaster(props) {
     console.log(props);
     const [results, setResults] = useState([])
+    
 
     const lat= localStorage.getItem('lat');
     const long = localStorage.getItem('long');
+    console.log('tm', lat, long)
 
-let apiUrl = `${baseUrl}lat=${lat}&long=${long}&appid=${key}`;
+
+    let apiUrl = `${baseUrl}lat=${lat}&lon=${long}&apikey=${key}`;
 
 console.log(apiUrl);
 
 let fetchMaster = () => {
-    let apiUrl = `${baseUrl}lat=${lat}&long=${long}&appid=${key}`;
+    //let apiUrl = `${baseUrl}lat=${lat}&lon=${long}&apikey=${key}`;
 
 
     fetch(apiUrl)
