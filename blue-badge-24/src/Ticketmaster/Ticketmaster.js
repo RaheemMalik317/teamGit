9b454400c1efa049596ef34db6c21e694b21c5db
RaheemMalik317 <82741663+RaheemMalik317@users.noreply.github.com>
2021-06-08 20:51:04 -0400

    more testing

--- blue-badge-24/src/Ticketmaster/Ticketmaster.js
+++ blue-badge-24/src/Ticketmaster/Ticketmaster.js
@@ -7,7 +7,11 @@ const key = '7drYHlAFPDVkMAGRvyAmhdDlCUWyZzGv'
 
 function Ticketmaster(props) {
     console.log(props);
-    const [results, setResults] = useState([])
+    const [results, setResults] = useState([]);
+    const [localStartDateTime, setLocalStartDateTime] = useState([]);
+    const [localStartEndDateTime, setLocalStartEndDateTime] = useState([]);
+    const [pageNumber, setPageNumber] = useState(0);
+    const [search, setSearch]= useState('')
     
 
     const lat= localStorage.getItem('lat');
@@ -19,14 +23,37 @@ function Ticketmaster(props) {
 
 console.log(apiUrl);
 
-let fetchMaster = () => {
-    //let apiUrl = `${baseUrl}lat=${lat}&lon=${long}&apikey=${key}`;
+let fetchResults = () => {
+    let url = `${baseUrl}lat=${lat}&lon=${long}&api-key=${key}&page=
+    ${pageNumber}&q=${search}`;
+    url = localStartDateTime ? url + `&begin_date=${localStartDateTime}` :url;
+    url = localStartEndDateTime ? url + `&end_date=${localStartEndDateTime}` : url;
 
 
     fetch(apiUrl)
     .then(res => res.json())
     .then(data => setResults(data.response))
     .catch(err => console.log(err));
+}
+
+const hadnleSubmit = (event) => {
+    event.preventDefault();
+    setPageNumber(0);
+    fetchResults();
+}
+
+const changePageNumber = (event, direction) => {
+    event.preventDefault()
+    if (direction === 'down') {
+        if (pageNumber > 0) {
+            setPageNumber(pageNumber - 1);
+            fetchResults();
+        }
+    }
+    if (direction === 'up') {
+        setPageNumber(pageNumber + 1);
+        fetchResults()
+    }
 }
 
 
