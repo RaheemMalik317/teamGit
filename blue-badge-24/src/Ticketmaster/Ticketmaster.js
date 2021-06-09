aa59864e056de7466dbeca4f095b4d9bcc4e5a53
RaheemMalik317 <82741663+RaheemMalik317@users.noreply.github.com>
2021-06-08 23:28:07 -0400

    pushing12

--- blue-badge-24/src/Ticketmaster/Ticketmaster.js
+++ blue-badge-24/src/Ticketmaster/Ticketmaster.js
@@ -1,79 +1,60 @@
-import React, {useState} from 'react'
-import {Button} from 'reactstrap'
+import React, { useState } from "react";
+import { Button } from "reactstrap";
 // import TicketmasterResults from './TicketmasterResults'
 
-const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?'
-const key = '7drYHlAFPDVkMAGRvyAmhdDlCUWyZzGv'
+const baseUrl = "https://app.ticketmaster.com/discovery/v2/events.json?";
+const key = "7drYHlAFPDVkMAGRvyAmhdDlCUWyZzGv";
 
 function Ticketmaster(props) {
-    console.log(props);
-    const [results, setResults] = useState([]);
-    const [localStartDateTime, setLocalStartDateTime] = useState([]);
-    const [localStartEndDateTime, setLocalStartEndDateTime] = useState([]);
-    const [pageNumber, setPageNumber] = useState(0);
-    const [search, setSearch]= useState('')
-    
+  console.log(props);
+  const [results, setResults] = useState([]);
+  const [localStartDateTime, setLocalStartDateTime] = useState([]);
+  const [localStartEndDateTime, setLocalStartEndDateTime] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [search, setSearch] = useState("");
 
-    const lat= localStorage.getItem('lat');
-    const long = localStorage.getItem('long');
-    console.log('tm', lat, long)
+  const lat = localStorage.getItem("lat");
+  const long = localStorage.getItem("long");
+  console.log("tm", lat, long);
 
+  let apiUrl = `${baseUrl}lat=${lat}&lon=${long}&apikey=${key}`;
 
-    let apiUrl = `${baseUrl}lat=${lat}&lon=${long}&apikey=${key}`;
+  console.log(apiUrl);
 
-console.log(apiUrl);
-
-let fetchResults = () => {
-    let url = `${baseUrl}lat=${lat}&lon=${long}&api-key=${key}&page=
-    ${pageNumber}&q=${search}`;
-    url = localStartDateTime ? url + `&begin_date=${localStartDateTime}` :url;
-    url = localStartEndDateTime ? url + `&end_date=${localStartEndDateTime}` : url;
-
+  let fetchResults = () => {
+    let url=`${baseUrl}&latlong=${lat},${long}&radius=25&unit=miles&locale=*&size=10&page=${pageNumber}&sort=date,asc&apikey=${key}`
 
     fetch(apiUrl)
-    .then(res => res.json())
-    .then(data => setResults(data.response))
-    .catch(err => console.log(err));
-}
+      .then((res) => res.json())
+      .then((data) => setResults(data.response))
+      .catch((err) => console.log(err));
+  };
 
-const hadnleSubmit = (event) => {
+  const hadnleSubmit = (event) => {
     event.preventDefault();
     setPageNumber(0);
     fetchResults();
-}
+  };
 
-const changePageNumber = (event, direction) => {
-    event.preventDefault()
-    if (direction === 'down') {
-        if (pageNumber > 0) {
-            setPageNumber(pageNumber - 1);
-            fetchResults();
-        }
+  const changePageNumber = (event, direction) => {
+    event.preventDefault();
+    if (direction === "down") {
+      if (pageNumber > 0) {
+        setPageNumber(pageNumber - 1);
+        fetchResults();
+      }
     }
-    if (direction === 'up') {
-        setPageNumber(pageNumber + 1);
-        fetchResults()
+    if (direction === "up") {
+      setPageNumber(pageNumber + 1);
+      fetchResults();
     }
-}
+  };
 
-
-
-
-
-
-
-
-
-
-
-    return (
-        <div >
-
-           <h3>Get Your Tickets Here!</h3>
-
-        </div>
-    );
-
+  return (
+    <div>
+      <h3>Get Your Tickets Here!</h3>
+    </div>
+  );
 }
 
 export default Ticketmaster;
